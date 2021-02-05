7bf9ed83fc63e7cfd52f119133fafd674a329045
robertmoore40 <robertcmoore92@gmail.com>
2021-02-05 11:13:32 -0500

    added return

--- src/Coin.js
+++ src/Coin.js
@@ -14,3 +14,6 @@ const Coin = ({
         <div className='coin-container'>
           <div className='coin-row'>
             <div className='coin'>
+            <img src={image} alt='crypto' />
+          <h1>{name}</h1>
+          <p className='coin-symbol'>{symbol}</p>
