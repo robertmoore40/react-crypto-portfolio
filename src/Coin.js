61677a52fdda996d85838a759ae3b9c7e8d047a3
robertmoore40 <robertcmoore92@gmail.com>
2021-02-05 11:14:22 -0500

    added price change

--- src/Coin.js
+++ src/Coin.js
@@ -23,3 +23,6 @@ const Coin = ({
           <p className='coin-volume'>${volume.toLocaleString()}</p>
 
 {priceChange < 0 ? (
+     <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+     ) : (
+       <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
