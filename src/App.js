b6b57c25bda1a6eac3e50349a4c3d7bb1ae9b4a7
robertmoore40 <robertcmoore92@gmail.com>
2021-02-08 11:12:09 -0500

    added api

--- src/App.js
+++ src/App.js
@@ -10,3 +10,6 @@ function App() {
   useEffect(() => {
     axios
       .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        )
+        .then(res => {
