ec23da149ff2a2975670fd631c31a3e96db69ac7
robertmoore40 <robertcmoore92@gmail.com>
2021-02-08 11:11:45 -0500

    added app function

--- src/App.js
+++ src/App.js
@@ -2,3 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Coin from './Coin';
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
