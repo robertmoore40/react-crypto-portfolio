dabe0238ceb9258e0ca02fd068a52aca86e3c5da
robertmoore40 <robertcmoore92@gmail.com>
2021-02-08 11:12:22 -0500

    added coin set

--- src/App.js
+++ src/App.js
@@ -13,3 +13,6 @@ function App() {
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
         )
         .then(res => {
+          setCoins(res.data);
+          console.log(res.data);
+        })
